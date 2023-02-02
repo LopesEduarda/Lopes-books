f872fb24c50e8cf568bca8d56f9265729303e8a4
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-02-02 16:40:41 -0300

    working on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import './App.css';
 import Logo from './components/Logo/index.js';
+import perfil from './img/perfil.svg';
+import sacola from './img/sacola.svg';
+
+const textOptions = ['Categorias', 'Favoritos', 'Minha Estante'];
+const icones = [perfil, sacola];
 
 function App() {
   return (
     <div className='App'>
-      <Logo />
+      <header className='App-header'>
+        <Logo />
+        <ul className='opcoes'>
+          {textOptions.map((option) => {
+            return <li className='opcao' key={option}>{option}</li>;
+          })}
+        </ul>
+
+        <ul className='icones'>
+          {icones.map((icone) => (
+            <li className='icone'><img src={icone} alt={icone} key={icone}></img></li>
+          ))}
+        </ul>
+      </header>
     </div>
   );
 }
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,12 +2,16 @@ import logo from '../../img/logo.svg';
 import './style.css'
 
 function Logo() {
-    return(
-        <div className='logo'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p><strong>Lopes</strong>Books</p>
-        </div>
-    )
+  return (
+    <div className='logo'>
+      <img
+        src={logo}
+        className='App-logo'
+        alt='logo'
+      />
+      <p><strong>Lopes</strong>Books</p>
+    </div>
+  )
 };
 
 export default Logo;
