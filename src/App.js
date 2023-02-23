8ff369a243449a53a479bd79d69840a60aa9952e
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-02-23 15:32:03 -0300

    initializing with styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
 import Header from './components/Header';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: rgb(238, 174, 202);
+    background: radial-gradient(circle, rgba(238, 174, 202, 1) 41%, rgba(148, 187, 233, 1) 100%);
+
+  li {
+      list-style-type: none;
+  }
+`
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,21 @@
-import './style.css';
 import Logo from '../../components/Logo/index.js';
 import OpcoesHeader from '../../components/OpcoesHeader';
 import Icones from '../../components/Icones';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    background-color: white;
+    display: flex;
+    justify-content: center;
+`
 
 function Header() {
     return (
-        <header className='App-header'>
+        <HeaderContainer>
             <Logo />
             <OpcoesHeader />
             <Icones />
-        </header>
+        </HeaderContainer>
     )
 }
 
